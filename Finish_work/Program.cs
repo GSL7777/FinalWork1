db0f984a0344e8e4007416ffe1371bf185a221cd
sergej gricel <siarhei_hrytsel@icloud.com>
2023-01-29 23:27:18 +0300

    Adedd creating data newArray

--- Finish_work/Program.cs
+++ Finish_work/Program.cs
@@ -1,5 +1,7 @@
 ﻿Console.WriteLine("Введите количество анализируемых данных:");
 int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Задайте минимальное количество символов:");
+// int m = Convert.ToInt32(Console.ReadLine());
 int m = 3;
 string y = "Change";
 int count = 0;
@@ -17,5 +19,18 @@ for (int i = 0; i < array.Length; i++)
    else array [i] = data;
 } 
 
+int k = n - count;
+string [] newArray = new string[k]; 
+int j = 0;
+for (int i = 0; i < array.Length; i++)
+{    
+   if (array[i] != y)
+   {
+   newArray [j] = array[i];
+   j++;  
+   }
+}
+
 Console.WriteLine();
-Console.WriteLine($"[{string.Join(", ",array)}]"); 
+// Console.WriteLine($"[{string.Join(", ",array)}]"); 
+Console.WriteLine($"[{string.Join(", ",newArray)}]");
