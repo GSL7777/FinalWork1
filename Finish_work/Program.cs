a17bf8949aebb217076d6ad01d26c81508e1b4a4
sergej gricel <siarhei_hrytsel@icloud.com>
2023-01-29 23:23:17 +0300

    Adedd creating data array

--- Finish_work/Program.cs
+++ Finish_work/Program.cs
@@ -3,3 +3,19 @@ int n = Convert.ToInt32(Console.ReadLine());
 int m = 3;
 string y = "Change";
 int count = 0;
+
+string [] array = new string [n];  
+for (int i = 0; i < array.Length; i++)
+{  
+   Console.WriteLine("Введите данные:"); 
+   string data = Convert.ToString(Console.ReadLine());
+   if (data.Length > m)
+   {
+   array [i] = y; 
+   count++;  
+   }
+   else array [i] = data;
+} 
+
+Console.WriteLine();
+Console.WriteLine($"[{string.Join(", ",array)}]"); 
